16af2785f03016283f473d33fe81531a70ccc1e6
kross <kirillchaikovsky8@gmail.com>
2025-07-23 22:50:03 +0300

    added ewigkeit's creative tab

--- src/main/java/com/ewigkeit/ewigkeit_core/EwigkeitCore.java
+++ src/main/java/com/ewigkeit/ewigkeit_core/EwigkeitCore.java
@@ -1,5 +1,8 @@
 package com.ewigkeit.ewigkeit_core;
 
+import com.ewigkeit.ewigkeit_core.forestry.feature.CustomApicultureBlocks;
+import com.ewigkeit.ewigkeit_core.forestry.feature.Frames;
+import com.ewigkeit.ewigkeit_core.forestry.hives.CustomBlockHiveType;
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
@@ -20,11 +23,14 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.function.Supplier;
+
+import static com.ewigkeit.ewigkeit_core.EwigkeitCreativeTab.CREATIVE_MODE_TABS;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(EwigkeitCore.MOD_ID)
@@ -40,7 +46,6 @@ public class EwigkeitCore
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
 
     // public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () ->
     //         new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
@@ -52,12 +57,7 @@ public class EwigkeitCore
     //         new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().nutrition(1).saturationMod(2f).build())));
 
     // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-    // public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-    //         .withTabsBefore(CreativeModeTabs.COMBAT)
-    //         .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-    //         .displayItems((parameters, output) -> {
-    //             output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-    //         }).build());
+    
 
     public EwigkeitCore(FMLJavaModLoadingContext context)
     {
@@ -68,8 +68,10 @@ public class EwigkeitCore
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
         CREATIVE_MODE_TABS.register(modEventBus);
+        
 
         MinecraftForge.EVENT_BUS.register(this);
+        
         
         // modEventBus.addListener(this::addCreative); // Register the item to a creative tab
 
@@ -80,24 +82,25 @@ public class EwigkeitCore
         // dictionary.put(Logger.class, () -> LOGGER);
         // Services = new ServiceLocator(new ServiceLocatorBindings(dictionary));
         LOGGER.info("EwigkeitCore loaded");
-        CreativeModeTabs.allTabs().forEach(x -> LOGGER.info(x.getDisplayName().getString()));
+        // CreativeModeTabs.allTabs().forEach(x -> LOGGER.info(x.getDisplayName().getString()));
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        // LOGGER.info("HELLO FROM COMMON SETUP");
-        //
-        // if (Config.logDirtBlock)
-        //     LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-        //
-        // LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-        //
-        // Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+        LOGGER.info("EwigkeitCore Common Setup");
+        
+        
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
             // event.accept(EXAMPLE_BLOCK_ITEM);
         }
+        // Hives
+        // for (CustomBlockHiveType type : CustomBlockHiveType.values()) {
+        //     if (type.getSpeciesId().toString().contains("mobees")) {
+        //         CREATIVE_MODE_TABS.accept(CustomApicultureBlocks.BEEHIVE.get(type));
+        //     }
+        // }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/ewigkeit/ewigkeit_core/EwigkeitCreativeTab.java
@@ -0,0 +1,44 @@
+package com.ewigkeit.ewigkeit_core;
+
+import com.ewigkeit.ewigkeit_core.forestry.feature.CustomApicultureBlocks;
+import com.ewigkeit.ewigkeit_core.forestry.feature.Frames;
+import com.ewigkeit.ewigkeit_core.forestry.hives.CustomBlockHiveType;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.*;
+
+public class EwigkeitCreativeTab {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS
+            = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, EwigkeitCore.MOD_ID);
+    
+    private static CreativeModeTab.Builder builder;
+    
+    public static RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("ewigkeit_tab",
+            () -> CreativeModeTab.builder()
+                    .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
+                    .title(Component.translatable("creativetab.ewigkeit"))
+                    .icon(() -> ForgeRegistries.ITEMS.getValue(ResourceLocation.parse("minecraft:iron_ingot")).getDefaultInstance())
+                    .displayItems(EwigkeitCreativeTab::addDisplayItems).build());
+    
+    public static List<Item> itemsToAdd = new ArrayList<Item>();
+    
+    private static void addDisplayItems(CreativeModeTab.ItemDisplayParameters parameters, CreativeModeTab.Output output) {
+        // Hives
+        for (CustomBlockHiveType type : CustomBlockHiveType.values()) {
+            if (type.getSpeciesId().toString().contains(EwigkeitCore.MOD_ID)) {
+                output.accept(CustomApicultureBlocks.BEEHIVE.get(type).item());
+            }
+        }
+        
+        // Frames
+        output.accept(Frames.FRAME_OBLIVION.item());
+    }
+}
